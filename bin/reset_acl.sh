23445889bf25db83aa156cf1f00de7c455255fe0
dinurajd <120137529+dinurajd@users.noreply.github.com>
2023-04-29 02:28:11 +0800

    Delete bin directory

--- bin/reset_acl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd $(dirname $0)
-cd ..
-export NX_HOME=$PWD
-export PATH=$PATH:/usr/bin:/usr/local/bin
-java -cp $NX_HOME/nxd.jar:$NX_HOME/lib/*: nxd.NxAdmin reset_acl
--- bin/tutorial.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start http://www.nxfilter.org/tutorial.html
